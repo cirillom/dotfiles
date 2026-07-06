b341f6dbaf13595dac03c319cb826e2cc52c3df2
inoa-mhcirillo <mhcirillo@inoa.com.br>
2026-07-06 15:46:13 -0300

    Add remote bootstrap execution script

---
+++ boostrap.ps1
@@ -0,0 +1,15 @@
+# 1. Define base Winget arguments to suppress interactive prompts
+$wingetArgs = @("--accept-package-agreements", "--accept-source-agreements", "--silent")
+
+# 2. Allocate runtimes via Winget
+winget install Git.Git @wingetArgs
+winget install twpayne.chezmoi @wingetArgs
+winget install Neovim.Neovim @wingetArgs
+winget install Nushell.Nushell @wingetArgs
+winget install marlocarlo.psmux @wingetArgs
+
+# 3. Reload environment variables into the current process memory to resolve newly installed binary paths
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
+
+# 4. Pull state and apply configurations
+chezmoi init https://github.com/cirillom/dotfiles --apply
